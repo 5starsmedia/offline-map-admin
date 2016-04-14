c9be7653c7644588fe417365a544ca1a95f0541a
Vitalii Savchuk <esvit666@gmail.com>
2016-04-14 14:35:23 +0300

    init

--- api/modules/posts/routes/data.js
+++ api/modules/posts/routes/data.js
@@ -71,6 +71,64 @@ var addPostForCategory = function(app, category, item, result, next) {
     });
 };
 
+function fillNearPlaces(list)
+{
+    var places = _.filter(list, function(item) {
+        return !!item.coordinates;
+    });
+    console.info(places);
+    _.each(places, function(item) {
+        var distances = [];
+
+        _.each(places, function(place) {
+            if (place.id == item.id) {
+                return;
+            }
+            distances.push({
+                id: place.id,
+                distance: getDistance(item, place)
+            });
+        });
+        distances = _.take(_.sortBy(distances, 'distance'), 5);
+
+        item.content.push({
+            type: "places",
+            items: _.pluck(distances, 'id')
+        });
+    });
+}
+
+function getDistance(place1, place2)
+{
+    function calcCrow(lat1, lon1, lat2, lon2)
+    {
+        var R = 6371; // km
+        var dLat = toRad(lat2-lat1);
+        var dLon = toRad(lon2-lon1);
+        var lat1 = toRad(lat1);
+        var lat2 = toRad(lat2);
+
+        var a = Math.sin(dLat/2) * Math.sin(dLat/2) +
+            Math.sin(dLon/2) * Math.sin(dLon/2) * Math.cos(lat1) * Math.cos(lat2);
+        var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+        var d = R * c;
+        return d;
+    }
+
+    // Converts numeric degrees to radians
+    function toRad(Value)
+    {
+        return Value * Math.PI / 180;
+    }
+
+    return calcCrow(
+        place1.coordinates[0],
+        place1.coordinates[1],
+        place2.coordinates[0],
+        place2.coordinates[1]
+    );
+}
+
 router.get('/', function (req, res, next) {
     async.auto({
         'categories': function (next) {
@@ -129,6 +187,7 @@ router.get('/', function (req, res, next) {
                 }
                 return item;
             });
+            fillNearPlaces(result);
             res.json(result);
         });
     });
