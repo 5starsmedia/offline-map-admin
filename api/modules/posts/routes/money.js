113298aed06ac298dc16a9d75a6a57d198601440
vitaliisavchuk <vitaliisavchuk@cloud.upwork.com>
2016-10-12 14:50:50 +0300

    add regions

--- api/modules/posts/routes/money.js
+++ api/modules/posts/routes/money.js
@@ -1,41 +1,23 @@
 'use strict';
 
 var express = require('express'),
-    moment = require('moment'),
-    _ = require('lodash'),
-    async = require('async'),
-    tmp = require('tmp'),
-    fs = require('fs'),
-    fstream = require('fstream'),
-    tar = require('tar'),
-    zlib = require('zlib'),
-    path = require('path'),
-    mongoose = require('mongoose'),
-  http = require('http'),
-    cheerio = require('cheerio'),
-    util = require('util'),
-    router = express.Router();
+    router = express.Router(),
+  request = require("request");
 
 
 router.get('/', function (req, res, next) {
-    var options = {
-        hostname: 'bank.gov.ua',
-        port: 433,
-        path: '/NBUStatService/v1/statdirectory/exchange?json',
-        method: 'GET'
-    };
 
-    var reqA = http.request(options, function(resA) {
-        console.info('res')
-        resA.setEncoding('utf8');
-        resA.on('data', function (data) {
-            res.json(data);
-        });
-    });
-    reqA.on('error', function(e) {
-        res.status(500).end();
-    });
-    reqA.end();
+    var url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+
+    request({
+        url: url,
+        json: true
+    }, function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            res.json(body) // Print the json response
+        }
+    })
 });
 
 module.exports = router;
